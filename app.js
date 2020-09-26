0967ed38e0fe8a47f2bd7635f4272ef881735d21
francomartino2003 <ch16.martino@gmail.com>
2020-09-25 20:11:39 -0500

    objeto de objetos

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 var atributos = ["numeros", "letras", "año1", "año2", "año3", "año4", "año5"];
 
-var materias = [
-  {nombre : "matematica",
-   attr : {numeros:true, letras:false, año1:true, año2: true, año3:true, año4:true, año5:true, iguales:0}
-  },
-  {nombre : "lengua",
-   attr : {letras:true, numeros:false, año1: true, año2:true, año3:true, año4: true, año5:true, iguales:0}
-  },
-  {nombre : "geografia",
-   attr : {letras:true, numeros:false, año1:true, año2:true, año3:true, año4:true, año5:false, iguales:0}
-  }
-];
+var materias = {
+  "matematica": {numeros:true, letras:false, año1:true, año2: true, año3:true, año4:true, año5:true, iguales:0},
+  "lengua": {letras:true, numeros:false, año1: true, año2:true, año3:true, año4: true, año5:true, iguales:0},
+  "geografia":{letras:true, numeros:false, año1:true, año2:true, año3:true, año4:true, año5:false, iguales:0}
+};
 
 var tu_materia = function(){
   var data = tomar_datos();
@@ -36,26 +30,46 @@ function tomar_datos(){
 
 function comparar_datos(data){
   for (var i in data) {
-      for (var j = 0; j < materias.length; j++) {
-        if (data[i] == materias[j].attr[i]) {
-          materias[j].attr.iguales++;
-        }
+    for (var f in materias) {
+      if (data[i] == materias[f][i]) {
+        materias[f].iguales++;
       }
-  }
-
-  mayor = [null,0]; // mayor = [indice materias, numero iguales]
-  for (var i = 0; i < materias.length; i++) {
-    if ((mayor[0] == null) || (materias[i].attr.iguales > mayor[1])) {
-      mayor[0] = i;
-      mayor[1] = materias[i].attr.iguales
     }
-    materias[i].attr.iguales = 0;
   }
-  return materias[mayor[0]].nombre;
+  key = null;
+  n_iguales = 0;
+  for (var i in materias) {
+    if ((key == null) || (materias[i].iguales > n_iguales)) {
+      key = i;
+      n_iguales = materias[i].iguales
+    }
+    materias[i].iguales = 0;
+  }
+  return key;
 };
 
 function agregar_materia(nombre, attr){
-  var length = materias.push({"nombre" : nombre, "attr" : attr});
-  materias[(length - 1)].attr.iguales = 0;
-  console.log(materias);
+  if (materias[nombre]) {
+    console.log("la materia ya existe");
+  }else {
+    materias[nombre] = attr;
+    materias[nombre].iguales = 0;
+  }
 };
+
+function agregar_atributo(materia,attr,valor){
+  if (atributos.includes(attr)) {
+    console.log("el atributo ya existe");
+  }else {
+    atributos.push(attr);
+    for (var i in materias) {
+      if (i == materia) {
+        materias[i][attr] = valor;
+      }else {
+        materias[i][attr] = null;
+      }
+    }
+  }
+};
+
+//{numeros:true, letras:false, año1:false, año2: false, año3:false, año4:true, año5:true, iguales:0}
