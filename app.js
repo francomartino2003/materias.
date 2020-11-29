99b2e42718264333b9c565d6d124846359a15927
francomartino2003 <ch16.martino@gmail.com>
2020-11-28 21:27:57 -0600

    listoo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-
 var atributos = null;
 var materias = null;
+var fileName = '';
 
 function handleFileSelect(){
   if (window.File && window.FileReader && window.FileList && window.Blob) {
@@ -17,6 +17,7 @@ function handleFileSelect(){
     alert("Please select a file before clicking 'Load'");
   } else {
     file = input.files[0];
+    fileName = file.name;
     fr = new FileReader();
     fr.onload = receivedText;
     fr.readAsText(file);
@@ -26,16 +27,43 @@ function handleFileSelect(){
 function receivedText() {
    var result = fr.result;
    var jsonVar = JSON.parse(result);
-   materias = jsonVar.materias;
-   atributos = jsonVar.atributos;
+   if (jsonVar.materias && jsonVar.atributos) {
+     materias = jsonVar.materias;
+     atributos = jsonVar.atributos;
+   }
  }
 
+function download() {
+  if (fileName) {
+    var a = document.createElement("a");
+    var globales = {"materias":materias, "atributos":atributos};
+    var str = JSON.stringify(globales); //tuve que agregar esto porque sino, cuando se descargaba, solamente aparecia "[object Object]" en el file. Supongo que me pasaba porque lo  estaba haciendo con un objeto en vez de un array
+    var file = new Blob([str], {
+      type: 'text/plain'
+    });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+  } else {
+    console.error("error");
+  }
+}
+
+
 var tu_materia = function(){
-  var data = tomar_datos();
-  var rta = comparar_datos(data);
-  var materia = rta[0];
-  var num_iguales = rta[1];
-  confirmar_respuesta(materia, num_iguales, data);
+  if (materias == null || atributos == null) {
+    alert("primero debes seleccionar un archivo");
+  }else {
+    var data = tomar_datos();
+    var rta = comparar_datos(data);
+    var materia = rta[0];
+    var num_iguales = rta[1];
+    var cambios = confirmar_respuesta(materia, num_iguales, data);
+    if (cambios) {
+      download(); // solo se descarga si el usuario agrego o modifico una materia o atributo de forma correcta.
+    }
+    location.reload(); // hace un refresh para que se limpien las variables globales y se tenga que agregar el ultimo file.
+ }
 };
 
 function tomar_datos(){
@@ -79,34 +107,47 @@ function agregar_materia(nombre, attr){
 };
 
 function agregar_atributo(materia){
+  var cambios = false;
   var attr = prompt("agrega un nuevo atributo para " + materia).toLowerCase();
-  if (atributos.includes(attr)) {
-    console.log("el atributo ya existe");
-  }else {
-    atributos.push(attr);
-    for (var i in materias) {
-        materias[i][attr] = null;
-    }
-    if (confirm("tu materia es de / esta en " + attr + " ?")) {
-      materias[materia][attr] = true;
+  if (attr) {
+    if (atributos.includes(attr)) {
+      console.log("el atributo ya existe");
+      cambios = false;
     }else {
-      materias[materia][attr] = false;
+      atributos.push(attr);
+      for (var i in materias) {
+          materias[i][attr] = null;
+      }
+      if (confirm("tu materia es de / esta en " + attr + " ?")) {
+        materias[materia][attr] = true;
+      }else {
+        materias[materia][attr] = false;
+      }
+      cambios = true;
     }
+  }else {
+    cambios = false;
   }
+  return cambios;
 };
 
 function confirmar_respuesta(materia, num_iguales, data){
+  var cambios = false;
   if (confirm("tu materia es " + materia + " ?")) {
     console.log(materia);
+    cambios = false;
   }else {
     var nueva_materia = prompt("cual era tu materia?").toLowerCase();
     if (nueva_materia) {
       agregar_materia(nueva_materia,data);
+      cambios = true;
       if (atributos.length == num_iguales) {
-        agregar_atributo(nueva_materia);
+        cambios = agregar_atributo(nueva_materia);
       }
     }else {
       console.log("no agregaste ninguna materia");
+      cambios = false;
     }
   }
+  return cambios;
 };
